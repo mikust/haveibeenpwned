54d93cc4e8512e12ff7f7ee8b67e4e007f6137a8
mikust <dont@think.so.meeeeh>
2019-06-08 15:43:18 +0300

    also prints out names for leaks and pastes

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -19,7 +19,7 @@ parser.add_argument("-f", dest="filename",
                   help="File to be checked with one email addresses per line")
 args = parser.parse_args()
 
-rate = 1.3                            # 1.3 seconds is a safe value that in most cases does not trigger rate limiting
+rate = 2                            # 1.3 seconds is a safe value that in most cases does not trigger rate limiting
 server = "haveibeenpwned.com"         # Website to contact
 sslVerify = True                      # Verify server certificate (set to False when you use a debugging proxy like BurpSuite)
 proxies = {                           # Proxy to use (debugging)
@@ -49,7 +49,7 @@ def main():
 
 def checkAddress(email):
     sleep = rate # Reset default acceptable rate
-    check = requests.get("https://" + server + "/api/v2/breachedaccount/" + email + "?includeUnverified=true",
+    check = requests.get("https://" + server + "/api/v2/breachedaccount/" + email + "?includeUnverified=true&truncateResponse=true",
                  proxies = proxies,
                  verify = sslVerify)
     if str(check.status_code) == "404": # The address has not been breached.
@@ -58,7 +58,30 @@ def checkAddress(email):
         return False
     elif str(check.status_code) == "200": # The address has been breached!
         print (FAILRED + "[!] " + email + " has been breached!" + ENDC)
+        json_response = check.json()
+        for n in json_response:
+            print (FAILRED + "  [+] Breach: " + n['Name'] + ENDC) 
         time.sleep(sleep) # sleep so that we don't trigger the rate limit
+        pasteCheck = requests.get("https://" + server + "/api/v2/pasteaccount/" + email,
+                 proxies = proxies,
+                 verify = sslVerify)
+        if str(pasteCheck.status_code) == "200":
+            json_response = pasteCheck.json()
+            for n in json_response:
+                 if 'Pastebin' in n['Source']:
+                     print (FAILRED + "  [+] Paste: https://pastebin.com/" + n['Id'] + ENDC)
+                 elif 'Pastie' in n['Source']:
+                     print (FAILRED + "  [+] Paste: https://pastie.org/pastes/" + n['Id'] + "/text" +  ENDC)
+                 elif 'Slexy' in n['Source']:
+                     print (FAILRED + "  [+] Paste: https://slexy.org/view/" + n['Id'] + ENDC)
+                 elif 'Ghostbin' in n['Source']:
+                     print (FAILRED + "  [+] Paste: https://ghostbin.com/paste/" + n['Id'] + ENDC)
+                 elif 'QuickLeak' in n['Source']:
+                     print (FAILRED + "  [+] Paste: https://www.quickleak.se/" + n['Id'] + ENDC)
+                 else:
+                     print (FAILRED + "  [+] Paste: " + n['Source'] + " " + n['Id'] + ENDC)
+            time.sleep(sleep) # sleep so that we don't trigger the rate limit
+
         return True
     elif str(check.status_code) == "429": # Rate limit triggered
         print (WARNING + "[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds" + ENDC)
